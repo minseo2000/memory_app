a94901bd195323f15988c57754e546bdf02a8734
minseo2000 <qlqlxks123@naver.com>
2023-04-12 02:03:47 +0900

    set firebase api

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:memory/screen/home_screen.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:memory/firebase_options.dart';
 void main() async {
 
 
@@ -10,6 +12,10 @@ void main() async {
   //네이버 지도 api 사용을 위한 설정
   await NaverMapSdk.instance.initialize(clientId: 'j8x23ixn8h');
 
+  //firebase api 사용을 위한 설정
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MaterialApp(
     home: HomeScreen(),
